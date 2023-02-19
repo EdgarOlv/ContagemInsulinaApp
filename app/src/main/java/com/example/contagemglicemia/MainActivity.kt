d38fabf28b0b01e950ce97c203573e3e5ada9256
Edgar Oliveira <edgaroliveira1258@gmail.com>
2023-02-19 10:47:05 -0300

    Criação da versão 1 básica do app
         - Adicionado a funcionalidade de apenas realizar a correção da glicemia.
         - Adicionado os 3 módulos: Glicemia, Configuração e relatório
         - Adicionado modulo de navegação pela parte inferior da tela

--- app/src/main/java/com/example/contagemglicemia/MainActivity.kt
+++ app/src/main/java/com/example/contagemglicemia/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.example.contagemglicemia
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.contagemglicemia.Config.ConfigFragment
+import com.example.contagemglicemia.Home.HomeFragment
+import com.example.contagemglicemia.Report.ReportFragment
+import com.example.contagemglicemia.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        replaceFragment(HomeFragment())
+
+        binding.bottomNavigationView.setOnItemSelectedListener {
+            when (it.itemId) {
+                R.id.glicemia -> replaceFragment(HomeFragment())
+                R.id.configuracao -> replaceFragment(ConfigFragment())
+                R.id.relatorio -> replaceFragment(ReportFragment())
+            }
+            true
+        }
+    }
+
+    private fun replaceFragment(fragment: Fragment) {
+        val fragmentManager = supportFragmentManager
+        val fragmentTransaction = fragmentManager.beginTransaction()
+        fragmentTransaction.replace(R.id.frame_layout, fragment)
+        fragmentTransaction.commit()
     }
 }
