6444b51086c143366cbd066d061677943cb93ba2
Edgar Oliveira <edgaroliveira1258@gmail.com>
2023-04-12 15:10:53 -0300

    Modulo inicial de alimentação no app

--- app/src/main/java/com/example/contagemglicemia/Modules/Home/HomeFragment.kt
+++ app/src/main/java/com/example/contagemglicemia/Modules/Home/HomeFragment.kt
@@ -1,15 +1,22 @@
 package com.example.contagemglicemia.Modules.Home
 
+import android.R
+import android.app.AlertDialog
 import android.content.Context
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.inputmethod.InputMethodManager
+import android.widget.ArrayAdapter
 import android.widget.CheckBox
 import androidx.fragment.app.Fragment
 import com.example.contagemglicemia.DAO.MyDatabaseManager
+import com.example.contagemglicemia.Model.Alimento
 import com.example.contagemglicemia.databinding.FragmentHomeBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 import java.math.RoundingMode
 import java.text.DecimalFormat
 
@@ -17,19 +24,40 @@ class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
     private lateinit var dbManager: MyDatabaseManager
+    var TAG = "HomeFragment"
+
+    // private lateinit var viewModel: HomeViewModel
     var glicemaAlvo = 0
     var fatorSensibilidade = 0
     var resultadoInsulina = 0.0
 
+    var listOfAliments: List<Alimento> = mutableListOf()
+    var alimentSelected = Alimento(0, "", "", 0)
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         dbManager = MyDatabaseManager(requireContext())
-        dbManager.getConfigData().forEach {
-            when(it.id){
-                1 -> fatorSensibilidade = it.value
-                2 -> glicemaAlvo = it.value
+        if (dbManager.getConfigData().isNullOrEmpty()) {
+            dbManager.startDataDatabase()
+            dbManager.getConfigData().forEach {
+                when (it.id) {
+                    1 -> fatorSensibilidade = it.value
+                    2 -> glicemaAlvo = it.value
+                }
+            }
+            listOfAliments = dbManager.getAlimentoData()
+        } else {
+            dbManager.getConfigData().forEach {
+                when (it.id) {
+                    1 -> fatorSensibilidade = it.value
+                    2 -> glicemaAlvo = it.value
+                }
             }
         }
+
+        // val viewModelFactory = HomeViewModel.Factory()
+        // viewModel = ViewModelProvider(this, viewModelFactory)            .get(HomeViewModel::class.java)
+
         // Carregar dados config (viewmodel)
         // Carregar Firebase
     }
@@ -52,6 +80,33 @@ class HomeFragment : Fragment() {
         checkBox.isChecked = true
 
         bindingCalcular(view)
+
+        binding.checkboxAlimentar.setOnClickListener {
+            if(binding.checkboxAlimentar.isChecked) {
+                selectAlimentation()
+            }
+        }
+
+        // val alimento = viewModel.getAlimentation(requireContext())
+    }
+
+    private fun selectAlimentation() {
+        // val opcoes = arrayOf("Opção 1", "Opção 2", "Opção 3", "Opção 4", "Opção 5", "Opção 6")
+        //CoroutineScope(Dispatchers.IO).launch {
+            val opcoes = dbManager.getAlimentoData()
+            val builder = AlertDialog.Builder(requireContext())
+
+            builder.setAdapter(
+                ArrayAdapter(requireContext(), R.layout.simple_list_item_1, opcoes.map { it.name })
+            ) { dialog, which ->
+                alimentSelected = opcoes[which]
+                binding.textViewTipoRefeicao.setText(alimentSelected.name)
+
+            }
+
+
+            builder.show()
+        //}
     }
 
     private fun bindingCalcular(view: View) {
