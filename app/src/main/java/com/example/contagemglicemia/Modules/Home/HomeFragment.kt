460ecffb17cdad4535aeca19279ed1a4730fd5a4
Edgar Oliveira <edgaroliveira1258@gmail.com>
2023-04-20 17:31:41 -0300

    -Correção do tema para rodar em varios dispositivos -Ajuste na forma de calculo da glicemia -Adicionado módulo de alimentação -Adicionado icone ao app

--- app/src/main/java/com/example/contagemglicemia/Modules/Home/HomeFragment.kt
+++ app/src/main/java/com/example/contagemglicemia/Modules/Home/HomeFragment.kt
@@ -14,9 +14,6 @@ import androidx.fragment.app.Fragment
 import com.example.contagemglicemia.DAO.MyDatabaseManager
 import com.example.contagemglicemia.Model.Alimento
 import com.example.contagemglicemia.databinding.FragmentHomeBinding
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 import java.math.RoundingMode
 import java.text.DecimalFormat
 
@@ -29,6 +26,7 @@ class HomeFragment : Fragment() {
     // private lateinit var viewModel: HomeViewModel
     var glicemaAlvo = 0
     var fatorSensibilidade = 0
+    var relacaoCarboidrato = 0
     var resultadoInsulina = 0.0
 
     var listOfAliments: List<Alimento> = mutableListOf()
@@ -43,6 +41,7 @@ class HomeFragment : Fragment() {
                 when (it.id) {
                     1 -> fatorSensibilidade = it.value
                     2 -> glicemaAlvo = it.value
+                    3 -> relacaoCarboidrato = it.value
                 }
             }
             listOfAliments = dbManager.getAlimentoData()
@@ -51,6 +50,7 @@ class HomeFragment : Fragment() {
                 when (it.id) {
                     1 -> fatorSensibilidade = it.value
                     2 -> glicemaAlvo = it.value
+                    3 -> relacaoCarboidrato = it.value
                 }
             }
         }
@@ -82,7 +82,7 @@ class HomeFragment : Fragment() {
         bindingCalcular(view)
 
         binding.checkboxAlimentar.setOnClickListener {
-            if(binding.checkboxAlimentar.isChecked) {
+            if (binding.checkboxAlimentar.isChecked) {
                 selectAlimentation()
             }
         }
@@ -91,33 +91,41 @@ class HomeFragment : Fragment() {
     }
 
     private fun selectAlimentation() {
-        // val opcoes = arrayOf("Opção 1", "Opção 2", "Opção 3", "Opção 4", "Opção 5", "Opção 6")
-        //CoroutineScope(Dispatchers.IO).launch {
-            val opcoes = dbManager.getAlimentoData()
-            val builder = AlertDialog.Builder(requireContext())
+        val opcoes = dbManager.getAlimentoData()
+        val builder = AlertDialog.Builder(requireContext())
 
-            builder.setAdapter(
-                ArrayAdapter(requireContext(), R.layout.simple_list_item_1, opcoes.map { it.name })
-            ) { dialog, which ->
-                alimentSelected = opcoes[which]
-                binding.textViewTipoRefeicao.setText(alimentSelected.name)
-
-            }
-
-
-            builder.show()
-        //}
+        builder.setAdapter(
+            ArrayAdapter(requireContext(), R.layout.simple_list_item_1, opcoes.map { it.name })
+        ) { dialog, which ->
+            alimentSelected = opcoes[which]
+            binding.textViewTipoRefeicao.setText(alimentSelected.name)
+        }
+        builder.show()
     }
 
     private fun bindingCalcular(view: View) {
         binding.buttonCalcular.setOnClickListener() {
             val valorDigitado = binding.editText.text.toString().toInt()
             val campoResultado = binding.textViewResultadoInsulina
+
             try {
                 if (valorDigitado != 0) {
-                    resultadoInsulina = ((valorDigitado.toDouble() - glicemaAlvo) / fatorSensibilidade)
-                    campoResultado.setText(roundOffDecimal(resultadoInsulina).toString())
-                    dbManager.insertGlycemia(valorDigitado)
+
+                    resultadoInsulina = 0.0
+                    if (binding.checkboxCorrigir.isChecked) {
+                        resultadoInsulina +=
+                            ((valorDigitado.toDouble() - glicemaAlvo) / fatorSensibilidade)
+                    }
+                    if (binding.checkboxAlimentar.isChecked) {
+                        resultadoInsulina +=
+                            (alimentSelected.qtd_carboidrato.toDouble() / relacaoCarboidrato)
+                    }
+                    if (binding.checkboxMalhar.isChecked) {
+                    }
+
+                    dbManager.insertGlycemia(valorDigitado, resultadoInsulina)
+                    campoResultado.setText(String.format("Aplicar %.2f UI", resultadoInsulina))
+
                     val imm =
                         requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
                     imm.hideSoftInputFromWindow(view.windowToken, 0)
